fdc01de0b7e5c72448e41d1e6daa40d41b10498f
sedihglow <jamesmross4@gmail.com>
2023-04-19 13:56:29 -0700

    finished up the basi getopt file

--- c/getopt_fam/main_getopt.c
+++ c/getopt_fam/main_getopt.c
@@ -7,16 +7,13 @@
 int main(int argc, char *argv[])
 {
 	int opt;
-	int did_it = 0;
 
 	while ((opt = getopt(argc, argv, OPT_STR)) != -1) {
 		switch (opt) {
 		case 'h':
 			/* print help menu/screen */
 			printf("Enter into the -h command!\n");
-			did_it = 1;
 		break;
-
 		default: /* '?' */
 			/* print error about some invalid arg */
 			printf("DEFAULTED ON THE ARGUMENT...\n");
@@ -24,16 +21,24 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	printf("\ndid_it = %d, argc = %d, optind = %d\n", did_it, argc, optind);
+	printf("\nargc = %d, optind = %d\n", argc, optind);
 
-
+	/*
+	 * If there is a argument that is not attached to a option (ex. -x)
+	 * optind will be placed at the first instance of a non option argument
+	 *
+	 * therefore, if we are expecting a non option argument optind would not
+	 * be >= argc. If there is no option argument optind is set = argc
+	 *
+	 * If no non option argument is expected, then this can be asjusted to
+	 * make sure optind is not pointing at something (a non option argument)
+	 * or can be removed all together if it doesnt matter for
+	 * implementation
+	 * */
 	if (optind >= argc) {
-		printf("Expected argument after options? optind >= argc.");
+		printf("Expected argument after options. optind >= argc.");
 		exit(EXIT_FAILURE);
 	}
-
-
-	/* If there is a argument that is not attached to a option (ex. -x) */
 
 	exit(EXIT_SUCCESS);
 }
